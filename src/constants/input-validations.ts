80910394688d1c2959a96ac39f59779d1f5bb1b7
anydayr <annyrondon491@gmail.com>
2024-10-26 18:10:20 -0400

    feat: Add constants for inputs validation messages

---
+++ src/constants/input-validations.ts
@@ -0,0 +1,11 @@
+const REQUIRED_VALIDATION_MESSAGE = 'Este campo es requerido';
+const INVALID_PASSWORD_VALIDATION_MESSAGE = 'La contraseña ingresada no es válida';
+const INVALID_USER_VALIDATION_MESSAGE = 'El usuario ingresado no es válido';
+const MIN_CHARACTERS_VALIDATION_PASSWORD = 'La contraseña debe tener un minimo de caracteres de';
+
+export {
+  REQUIRED_VALIDATION_MESSAGE,
+  INVALID_PASSWORD_VALIDATION_MESSAGE,
+  MIN_CHARACTERS_VALIDATION_PASSWORD,
+  INVALID_USER_VALIDATION_MESSAGE
+};
