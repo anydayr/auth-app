66b5857a306a6c58cb5af4bd59828fe164af361d
anydayr <annyrondon491@gmail.com>
2024-10-28 00:25:17 -0400

    feat: Create Edit Component

---
+++ src/pages/private/dashboard/Edit.tsx
@@ -0,0 +1,57 @@
+import { Container, Grid2 } from '@mui/material';
+import { Input, InputType } from '../../../components/TextField';
+import Button from '../../../components/Button';
+import useEditPlaceholders from './hooks/useEditPlaceholders';
+
+const EditPlaceholder = () => {
+  const { register, errors, onSubmit, handleSubmit, handleReset } = useEditPlaceholders();
+
+  return (
+    <Container style={{ maxWidth: '600px' }}>
+      <Grid2 container sx={{ p: '1rem' }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid2 container gap={2} sx={{ mb: '2rem' }}>
+            <Grid2 size={12}>
+              <Input
+                inputProps={{ defaultValue: '' }}
+                label="Título"
+                type={InputType.TEXT}
+                name="title"
+                register={register}
+                errors={errors}
+              />
+            </Grid2>
+            <Grid2 size={12}>
+              <Input
+                inputProps={{ defaultValue: '' }}
+                label="Descripción"
+                type={InputType.TEXT}
+                name="body"
+                register={register}
+                errors={errors}
+              />
+            </Grid2>
+          </Grid2>
+          <Grid2 container gap={2}>
+            <Grid2 size={4}>
+              <Button type="submit" variant="contained" color="primary" fullWidth>
+                Editar
+              </Button>
+            </Grid2>
+            <Grid2 size={4}>
+              <Button
+                type="button"
+                variant="contained"
+                color="secondary"
+                fullWidth
+                onClick={() => handleReset()}>
+                Limpiar
+              </Button>
+            </Grid2>
+          </Grid2>
+        </form>
+      </Grid2>
+    </Container>
+  );
+};
+export default EditPlaceholder;
