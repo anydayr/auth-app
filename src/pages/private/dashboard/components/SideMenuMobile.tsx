7146769cb57922656c72000b85161edd180c6a2a
anydayr <annyrondon491@gmail.com>
2024-10-31 17:02:47 -0400

    feat: add menu open property

--- src/pages/private/dashboard/components/SideMenuMobile.tsx
+++ src/pages/private/dashboard/components/SideMenuMobile.tsx
@@ -46,7 +46,7 @@ export default function SideMenuMobile({ open, toggleDrawer }: SideMenuMobilePro
         }}>
         <Stack direction="row" sx={{ p: 2, pb: 0, gap: 1 }}>
           <Stack direction="row" sx={{ gap: 1, alignItems: 'center', flexGrow: 1, p: 1 }}>
-            <AccountMenu />
+            <AccountMenu menu={false} />
           </Stack>
           <MenuButton showBadge>
             <NotificationsRoundedIcon />
